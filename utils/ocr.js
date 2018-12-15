170dafbadb54826dad150724c42ea0cf131d175f
Zhi Yang Ten <tenzy@seagroup.com>
2018-12-15 12:43:32 +0800

    ! fixed eslint issues

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-module.exports = function(imageUri) {
+module.exports = (imageUri) => {
   return fetch(
     `https://vision.googleapis.com/v1/images:annotate?key=${
       process.env.GOOGLE_API_KEY
@@ -12,18 +12,18 @@ module.exports = function(imageUri) {
           {
             image: {
               source: {
-                imageUri
-              }
+                imageUri,
+              },
             },
             features: [
               {
-                type: 'TEXT_DETECTION'
-              }
-            ]
-          }
-        ]
-      })
-    }
+                type: 'TEXT_DETECTION',
+              },
+            ],
+          },
+        ],
+      }),
+    },
   )
     .then(res => res.json())
     .then(res => res.responses[0].fullTextAnnotation.text)
