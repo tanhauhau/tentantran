50593f89351355312bd9d10d27dc0d232cead801
Zhi Yang Ten <tenzy@seagroup.com>
2018-12-15 16:10:07 +0800

    ! added more response

--- utils/replies.js
+++ utils/replies.js
@@ -2,5 +2,9 @@ module.exports = {
   lazy: [
     'I am busy hacking in super silly hackathon, try again later.',
     "Have you been to gojek's office? The view is nice.",
+    "I don't feel like doing any translation...",
+    "I'm sleepy, can I do later",
+    "Ain't nobody got time fo dat",
+    'Try again later...',
   ],
 };
