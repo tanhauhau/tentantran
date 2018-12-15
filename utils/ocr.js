60d99acb05ec68a6b1983dd6899f15110b204538
Tan Li Hau <lhtan93@gmail.com>
2018-12-15 12:56:44 +0800

    ! add debug

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,6 +1,6 @@
 const fetch = require('node-fetch');
 
-module.exports = (imageUri) => {
+module.exports = imageUri => {
   return fetch(
     `https://vision.googleapis.com/v1/images:annotate?key=${
       process.env.GOOGLE_API_KEY
@@ -12,20 +12,36 @@ module.exports = (imageUri) => {
           {
             image: {
               source: {
-                imageUri,
-              },
+                imageUri
+              }
             },
             features: [
               {
-                type: 'TEXT_DETECTION',
-              },
-            ],
-          },
-        ],
-      }),
-    },
+                type: 'TEXT_DETECTION'
+              }
+            ]
+          }
+        ]
+      })
+    }
   )
     .then(res => res.json())
+    .then(res => {
+      debug(imageUri, res);
+      return res;
+    })
     .then(res => res.responses[0].fullTextAnnotation.text)
     .catch(_ => '');
 };
+
+function debug(imageUri, res) {
+  try {
+    console.log(imageUri);
+    for (const response of res.responses) {
+      console.log(response.fullTextAnnotation.text);
+    }
+    console.log();
+  } catch (e) {
+    // pass
+  }
+}
