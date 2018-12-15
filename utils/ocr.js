9f2426d8659984df38d7fa4f96b2cd4cc3195c1e
Tan Li Hau <lhtan93@gmail.com>
2018-12-15 13:13:15 +0800

    ! fix eslint warnings

--- utils/ocr.js
+++ utils/ocr.js
@@ -1,42 +1,9 @@
 const fetch = require('node-fetch');
 
-module.exports = imageUri => {
-  return fetch(
-    `https://vision.googleapis.com/v1/images:annotate?key=${
-      process.env.GOOGLE_API_KEY
-    }`,
-    {
-      method: 'POST',
-      body: JSON.stringify({
-        requests: [
-          {
-            image: {
-              source: {
-                imageUri
-              }
-            },
-            features: [
-              {
-                type: 'TEXT_DETECTION'
-              }
-            ]
-          }
-        ]
-      })
-    }
-  )
-    .then(res => res.json())
-    .then(res => {
-      debug(imageUri, res);
-      return res;
-    })
-    .then(res => res.responses[0].fullTextAnnotation.text)
-    .catch(_ => '');
-};
-
 function debug(imageUri, res) {
   try {
     console.log(imageUri);
+    console.log(res);
     for (const response of res.responses) {
       console.log(response.fullTextAnnotation.text);
     }
@@ -45,3 +12,30 @@ function debug(imageUri, res) {
     // pass
   }
 }
+
+module.exports = imageUri => fetch(`https://vision.googleapis.com/v1/images:annotate?key=${process.env.GOOGLE_API_KEY}`, {
+  method: 'POST',
+  body: JSON.stringify({
+    requests: [
+      {
+        image: {
+          source: {
+            imageUri,
+          },
+        },
+        features: [
+          {
+            type: 'TEXT_DETECTION',
+          },
+        ],
+      },
+    ],
+  }),
+})
+  .then(res => res.json())
+  .then((res) => {
+    debug(imageUri, res);
+    return res;
+  })
+  .then(res => res.responses[0].fullTextAnnotation.text)
+  .catch(() => '');
